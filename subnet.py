3bcc871019d4cd78ec1bf385f0d41d3753fbe060
fakirpoo <74319215+fakirpoo@users.noreply.github.com>
2023-10-30 14:22:21 -0400

    Add files via upload

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 18271
+
+#problem 1
+p1Network = '249.210.116.64'
+p1Broadcast = '249.210.116.127'
+p1First = '249.210.116.65'
+p1Last =  '249.210.116.126'
+
+#problem 2
+p2Network = '160.195.78.0'
+p2Broadcast = '160.195.78.255'
+p2First = '160.195.78.1'
+p2Last = '160.195.78.254'
+
+#problem 3
+p3Network = '71.180.40.160'
+p3Broadcast = '71.180.40.191'
+p3First = '71.180.40.161'
+p3Last = '71.180.40.190'
+
+#problem 4
+p4Network = '238.165.1.128'
+p4Broadcast = '238.165.1.255'
+p4First = '238.165.1.129'
+p4Last = '238.165.1.254'
+
+#problem 5
+p5Network = '149.150.219.240'
+p5Broadcast = '149.150.219.255'
+p5First = '149.150.219.241'
+p5Last = '149.150.219.254'
+
+#problem 6
+p6Network = '60.135.181.0'
+p6Broadcast = '60.135.181.63'
+p6First = '60.135.181.1'
+p6Last =  '60.135.181.62'
+
+#problem 7
+p7Network = '227.121.143.80'
+p7Broadcast = '227.121.143.87'
+p7First = '227.121.143.81'
+p7Last = '227.121.143.86'
+
+#problem 8
+p8Network = '138.106.104.96'
+p8Broadcast = '138.106.104.127'
+p8First = '138.106.104.97'
+p8Last = '138.106.104.126'
+
+#problem 9
+p9Network = '49.91.66.0'
+p9Broadcast = '49.91.66.255'
+p9First = '49.91.66.1'
+p9Last = '49.91.66.254'
+
+#problem 10
+p10Network = '216.76.28.208'
+p10Broadcast = '216.76.28.223'
+p10First = '216.76.28.209'
+p10Last = '216.76.28.222'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
